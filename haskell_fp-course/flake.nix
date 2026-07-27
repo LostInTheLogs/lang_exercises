9fca29ef2a7ad5c7c1ce7d73f2861c24a293b7e5
LostInTheLogs <110467150+LostInTheLogs@users.noreply.github.com>
2026-07-27 18:34:14 +0200

    list_begin

--- haskell_fp-course/flake.nix
+++ haskell_fp-course/flake.nix
@@ -2,7 +2,6 @@
   description = "A Hello World in Haskell with a dependency and a devShell (using flake-parts)";
 
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs.url = "nixpkgs/nixos-26.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
@@ -14,7 +13,14 @@
     ...
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "x86_64-darwin"];
+      systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+
+      flake = {
+        overlay = final: prev: {
+          # Attach directly to the top-level pkgs scope
+          haskell-app = final.haskell.packages.ghc984.callCabal2nix "haskell-app" ./. {};
+        };
+      };
 
       perSystem = {
         config,
@@ -24,15 +30,36 @@
         system,
         ...
       }: let
+        hp = pkgs.haskell.packages.ghc984;
       in {
-        devShells.default = pkgs.haskellPackages.shellFor {
-          packages = p: [];
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            self.overlay
+          ];
+          config = {};
+        };
+
+        packages = {
+          haskell-app = pkgs.haskell-app;
+          default = pkgs.haskell-app;
+        };
+
+        checks = self'.packages;
+
+        devShells.default = hp.shellFor {
+          # Reference pkgs.haskell-app here since overlay put it on top-level pkgs
+          packages = p: [pkgs.haskell-app];
           withHoogle = true;
-          buildInputs = with pkgs.haskellPackages; [
-            haskell-language-server
-            dap
-            ghcid
-            cabal-install
+          buildInputs = [
+            (pkgs.haskell-language-server.override {
+              supportedGhcVersions = [
+                "984"
+              ];
+            })
+            # pkgs.dap
+            hp.ghcid
+            hp.cabal-install
           ];
         };
       };
